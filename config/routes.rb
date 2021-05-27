33bf617d05bee398c18bca874d9adb3610580d54
Brian-Demon <BrianWMatejka@gmail.com>
2021-05-27 15:44:02 -0500

    Routes for registration and session management

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :users
+
+  get "register", to: "registrations#new"
+  post "register", to: "registrations#create"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
 end
