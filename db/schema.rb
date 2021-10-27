dd907c6f327da0b21882abb812e4a0db0236b8c2
Brian-Demon <BrianWMatejka@gmail.com>
2021-10-27 18:29:18 -0500

    Migration: - change the name of start/end to shift_start/shift_end - This was causing an issue with "end" being a reserved word

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_220747) do
+ActiveRecord::Schema.define(version: 2021_10_27_232533) do
 
   create_table "exclusions", force: :cascade do |t|
     t.datetime "start_at"
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(version: 2021_10_27_220747) do
 
   create_table "shifts", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.datetime "start"
-    t.datetime "end"
+    t.datetime "shift_start"
+    t.datetime "shift_end"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "schedule_id", null: false
