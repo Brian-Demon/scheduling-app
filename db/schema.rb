d40c9cbfc76783f1ce33a7c43f1aef6aee61e409
Brian-Demon <BrianWMatejka@gmail.com>
2021-11-22 12:27:07 -0600

    Add `uid` & `provider` columns to `user` model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_232533) do
+ActiveRecord::Schema.define(version: 2021_10_28_201500) do
 
   create_table "exclusions", force: :cascade do |t|
     t.datetime "start_at"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2021_10_27_232533) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "role"
+    t.string "provider"
+    t.string "uid"
   end
 
 end
