7b9687161b11a3cb0bf39d2d7fa81b4dbb3c32c8
Brian-Demon <BrianWMatejka@gmail.com>
2021-11-23 16:20:01 -0600

    Update routes.rb
    To avoid a Cad eye twitch

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :users do
-    get "account", on: :member, to: "users#show"
+    get "account", to: "users#show", on: :member
     patch "account", to: "users#update", on: :member
   end
 
