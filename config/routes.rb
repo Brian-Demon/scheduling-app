084f2730c0c6c5362280a50e742467395a240fee
Demon <BrianWMatejka@gmail.com>
2022-06-20 15:31:26 -0500

    Re-org Controllers & Setup React Views: `application.thml.erb`:   - add new JS pack tag   - remove rails partial renders re-org controllers:     - Create new directories: `app/controllers/api/v1`   - delete `home_controller.rb` (now `pages_controller.rb`)   - move remaining existing controllers to new `/api/v1` directory `App.js`:   - create `App.js` that will act as the parent component `index.jsx`:   - rename `hello-react.jsx` to `index.jsx`   - now accpets `App` parent component as parent view new (main) view:   - create `index.html.erb` `routes.rb`:   - for now, comment out all old rails routes   - set new `root`   - setup new namespaces to match new controller directories

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,30 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  root "pages#index"
 
-  resources :users do
-    get "account", to: "users#show", on: :member
-    patch "account", to: "users#update", on: :member
+  namespace :api do
+    namespace :v1 do
+      
+    end
   end
+  # resources :users do
+  #   get "account", to: "users#show", on: :member
+  #   patch "account", to: "users#update", on: :member
+  # end
 
-  get "register", to: "registrations#new"
-  post "register", to: "registrations#create"
+  # get "register", to: "registrations#new"
+  # post "register", to: "registrations#create"
 
-  get "login", to: "sessions#new"
-  get "/auth/:provider/callback", to: "sessions#create"
-  post "login", to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
+  # get "login", to: "sessions#new"
+  # get "/auth/:provider/callback", to: "sessions#create"
+  # post "login", to: "sessions#create"
+  # delete "logout", to: "sessions#destroy"
 
-  get "password/reset", to: "password_resets#new"
-  post "password/reset", to: "password_resets#create"
-  get "password/reset/edit", to: "password_resets#edit"
-  patch "password/reset/edit", to: "password_resets#update"
+  # get "password/reset", to: "password_resets#new"
+  # post "password/reset", to: "password_resets#create"
+  # get "password/reset/edit", to: "password_resets#edit"
+  # patch "password/reset/edit", to: "password_resets#update"
 
-  resources :shifts
+  # resources :shifts
   
-  resources :schedules
+  # resources :schedules
 end
