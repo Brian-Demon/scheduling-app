8f473c79907aa3abacc9d1519bb4a971bdac9ead
Demon <BrianWMatejka@gmail.com>
2022-06-28 15:11:12 -0500

    Initial RestAPI Changes: Update sessions and user controllers to render json only Update Routes for new directories and API refactor

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,29 +1,47 @@
-class SessionsController < ApplicationController
-  def create
-    auth = request.env["omniauth.auth"]
-    user = auth ? authenticate_with_omniauth(auth) : authenticate_with_form(params)
-    
-    if user
-      session[:user_id] = user.id
-      redirect_to root_path, notice: "Welcome #{user.first_name}!"
-    else
-      flash[:alert] = "Invalid email or password"
-      render :new
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      def create
+        auth = request.env["omniauth.auth"]
+        user = auth ? authenticate_with_omniauth(auth) : authenticate_with_form(session_params)
+        
+        if user
+          if session[:user_id] == user.id
+            render json: { error: 'Already logged in' }, status: 422
+          else
+            session[:user_id] = user.id
+            render json: { message: "#{user.employee_name} logged in" }
+          end
+        else
+          render json: { error: 'Session not created' }, status: 422
+        end
+      end
+
+      def destroy
+        if session[:user_id].nil?
+          render json: { error: 'No current session' }, status: 422
+        else
+          session[:user_id] = nil
+          render json: { message: 'Logged out' }
+        end
+      end
+
+      def authenticate_with_omniauth(auth)
+        User.find_or_create_with_omniauth(auth)
+      end
+
+      def authenticate_with_form(params)
+        user = User.find_by(email: params[:email])
+        return false unless user.present?
+        user.authenticate(params[:password])
+      end
+
+      private
+
+      def session_params
+        params.require(:session).permit(:email, :password, :password_confirmation)
+      end
     end
-  end
-
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_path, notice: "Logged out"
-  end
-
-  def authenticate_with_omniauth(auth)
-    User.find_or_create_with_omniauth(auth)
-  end
-
-  def authenticate_with_form(params)
-    user = User.find_by(email: params[:email])
-    return false unless user.present?
-    user.authenticate(params[:password])
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,28 +1,63 @@
-class UsersController < ApplicationController
-  before_action :set_user
+module Api
+  module V1
+    class UsersController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      before_action :set_user, only: [:show, :update]
 
-  def show
-  end
+      def new
+        @user = User.new
+      end
 
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_back(fallback_location: @user, notice: "Account successfully updated.") }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :account, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: { message: "#{user.employee_name} Successfully Created" }
+        else
+          render json: { error: 'Unable to create user' }, status: 422
+        end
+      end
+
+      def index
+        users = User.all
+        render json: UserSerializer.new(users).serialized_json
+      end
+
+      def show
+        user = User.find(user_params[:id])
+        if user
+          render json: UserSerializer.new(user).serialized_json
+        else
+          render json: { error: 'User not found' }, status: 422
+        end
+      end
+
+      def update
+        if user.update(user_params)
+          render json: { message: "#{user.employee_name} Successfully Update" }
+        else
+          render json: { error: 'Unable to update user' }, status: 422
+        end
+      end
+
+      def destroy
+        user = User.find(params[:id])
+        user_name = user.employee_name if user
+        if user.destroy
+          render json: { message: "#{user_name} Successfully Deleted" }
+        else
+          render json: { error: 'User not found' }, status: 422
+        end
+      end
+
+      private
+
+      def set_user
+        @user = User.find(params[:id])
+      end
+
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
       end
     end
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :title)
   end
 end
