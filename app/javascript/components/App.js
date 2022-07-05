c588185ab87414f996a29cb17e57bd440751e99b
Demon <BrianWMatejka@gmail.com>
2022-07-05 14:45:07 -0500

    - remove unused `Login` component - remove unused axios import

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
-import Login from './pages/Login'
 
 const App = () => {
   return(
     <Routes>
       <Route exact path="/" element={ <Landing/> } />
-      <Route exact path="/login" element={ <Login/> } />
     </Routes>
   )
 }
