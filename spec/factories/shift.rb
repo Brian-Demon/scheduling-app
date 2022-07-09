05878252dd60ad761798287662ff627d36763590
Demon <BrianWMatejka@gmail.com>
2022-07-09 17:23:18 -0500

    Update shift times in factory

--- spec/factories/shift.rb
+++ spec/factories/shift.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :shift do
     user { FactoryBot.create(:user) }
     schedule { FactoryBot.create(:schedule) }
-    shift_start { Time.parse("September 20 18:00") }
-    shift_end { Time.parse("September 21 02:00") }
+    shift_start { Time.parse("September 1, 2022 18:00") }
+    shift_end { Time.parse("September 2, 2022 02:00") }
   end
 end
