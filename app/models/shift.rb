4b22a574b52ba3f08d837066ea692e1f519ad9ef
Demon <BrianWMatejka@gmail.com>
2022-07-09 19:20:17 -0500

    Additional Shift Validations: `shift.rb`:   - add custom validations via `validate_shift` `shift_spec.rb`:   - fix spec naming issue   - add expectations for custom Shift errors

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,12 +2,29 @@ class Shift < ApplicationRecord
   belongs_to :user
   belongs_to :schedule
   
-  validates :user, presence: true
-  validates :schedule, presence: true
-  validates :shift_start, presence: true
-  validates :shift_end, presence: true
+  validate :validate_shift
 
   def format_shift(type)
     type.strftime("%b %d, %Y @ %H:%M")
+  end
+
+  private
+
+  def validate_shift
+    if !user.present?
+      errors.add(:user, "must be present")
+    elsif !schedule.present?
+      errors.add(:schedule, "must be present")
+    elsif !shift_start.present?
+      errors.add(:shift_start, "must be present")
+    elsif !shift_end.present?
+      errors.add(:shift_end, "must be present")
+    elsif shift_start > shift_end
+      errors.add(:shift_end, "must be after the shift start date")
+    elsif shift_start < schedule.first_day
+      errors.add(:shift_start, "must fall within the schedule dates")
+    elsif shift_end > schedule.last_day
+      errors.add(:shift_end, "must fall within the schedule dates")
+    end
   end
 end
--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -5,24 +5,28 @@ RSpec.describe Shift, type: :model do
     expect(shift).to be_valid
   end
 
-  it "is not valid without a shift_start" do
+  it "is not valid without a user" do
     shift.user = nil
     expect(shift).to_not be_valid
+    expect("User must be present").to eq(shift.errors.full_messages.last)
   end
 
-  it "is not valid without a shift_start" do
+  it "is not valid without a schedule" do
     shift.schedule = nil
     expect(shift).to_not be_valid
+    expect("Schedule must be present").to eq(shift.errors.full_messages.last)
   end
 
   it "is not valid without a shift_start" do
     shift.shift_start = nil
     expect(shift).to_not be_valid
+    expect("Shift start must be present").to eq(shift.errors.full_messages.last)
   end
 
   it "is not valid without a shift_end" do
     shift.shift_end = nil
     expect(shift).to_not be_valid
+    expect("Shift end must be present").to eq(shift.errors.full_messages.last)
   end
 
   it "returns the formatted shift_start using format_shift" do
