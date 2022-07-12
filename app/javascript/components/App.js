823aa7e050f72d6cfbeb9bdccbefd786cc2ef578
Demon <BrianWMatejka@gmail.com>
2022-07-12 18:57:46 -0500

    Add Test Component for Dev: - test component to ensure proper react routing and navbar function - will be depricated once more compnents for the navbar are created

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Landing from './pages/Landing'
 import NavBar from './Navbar'
+import Test from './pages/Test'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Landing />} />
+        <Route exact path="/test" element={<Test />} />
       </Routes>
     </>
   )
